aa09f22d4de2613f4154de3bc20a4bf1da3d5a81
André Luferat <contato@luferat.net>
2025-06-16 21:36:41 -0300

    Update my_drawer.dart

--- lib/template/my_drawer.dart
+++ lib/template/my_drawer.dart
@@ -12,9 +12,42 @@ class MyDrawer extends StatelessWidget {
             child: ListView(
               children: [
                 // Cabeçalho do Menu (opcional)
-                const DrawerHeader(
+                DrawerHeader(
                   decoration: BoxDecoration(color: Colors.blue),
-                  child: Text('Drawer Header'),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsGeometry.fromLTRB(0, 0, 15, 0),
+                        child: Icon(
+                          Icons.account_circle,
+                          size: 54,
+                          color: Colors.white,
+                        ),
+                      ),
+                      InkWell(
+                        child: Text(
+                          'Faça Login',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onTap: () => {
+                          Navigator.pop(context),
+                          Navigator.pushNamed(context, '/detalhes'),
+                        },
+                      ),
+                      Spacer(),
+                      InkWell(
+                        child: Text(
+                          'Cadastre-se',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onTap: () => {
+                          Navigator.pop(context),
+                          Navigator.pushNamed(context, '/detalhes'),
+                        },
+                      ),
+                      Spacer(),
+                    ],
+                  ),
                 ),
 
                 // Exemplo de item de menu para uma rota
